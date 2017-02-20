66bc200e6239ba201ec506745f7d7f06f8fd70dd
Alex Ívar <alexivar65@gmail.com>
2017-02-20 19:37:08 +0000

    mo js could work know

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express")
 var app = express()
 const PORT = 3000
 app.use(express.static('public'))
+app.use('/scripts', express.static(__dirname + '/node_modules/mo-js'));
 
 app.get("/", function(req, res){
 	res.sendFile('index.html' , { root : __dirname});
