b53d3a84d8eb0f1045fdee88fe743c323881a833
Alex Ívar <alexivar65@gmail.com>
2017-02-23 00:23:25 +0000

    changed clock function

--- public/timer.js
+++ public/timer.js
@@ -1,4 +1,13 @@
+/*document.addEventListener( 'click', function (e) {
+  $('#myModal').modal('show')
+});*/
+
+
+
+
+
 const circ = new mojs.Shape({
+  className: "theClockCircle",
   radius: 200 ,
   fill:'none',
   stroke: { 'orange' : 'magenta' },
@@ -11,25 +20,61 @@ const circ = new mojs.Shape({
 
 }).play();
 
+$(document).ready(function() {
+  $('.theClockCircle').click(function() {
+   $("#myModal").modal("show")
+  });
+});
+
+console.log(circ);
 var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
 
-function countDown(){
+function countDown(min, sec){
+  $("#myModal").modal("hide")
+  console.log("minutes: " + min)
+  console.log("seconds: " + sec)
+  var w = new Date();
+  const makeTime = w.getHours() + ":" + (w.getMinutes() + parseInt(min)) + ":" + w.getSeconds();
+  const test = new Date('February 23, 2017 ' + makeTime);
   var timeinterval = setInterval(function(){
-
-  var end = new Date('February 20, 2017 19:00:00');
   var now = new Date();
 
-  var distance = end.getTime() - now.getTime();
-  console.log(distance);
-  if(distance <= 0){
-    document.getElementById("theClock").innerHTML = "Samkoman hefst núna!"
+  //var end = new Date('February 22, 2017 17:00:00');
+  //var end = (now.getTime() + min*60000);
+  //var now = new Date();
+  var m = now.getMinutes();
+  var t = m + parseInt(min);
+  console.log("m: " + t);
+  //var makeTime = now.getHours() + ":" + (now.getMinutes() + parseInt(min)) + ":" + now.getSeconds();
+  console.log("makeTime: " + makeTime);
+
+  console.log(test);
+  console.log("now: " + now);
+
+  var distance = test.getTime() - now.getTime();
+  if(distance <= 1){
+    document.getElementById("theClock").innerHTML = "!!!"
     return;
   }
   var minutes = Math.floor((distance % _hour) / _minute);
   var seconds = Math.floor((distance % _minute) / _second);
-  document.getElementById("theClock").innerHTML = pad(minutes) + ":" + pad(seconds);
+  console.log(seconds);
+  console.log(minutes);
+  if(minutes === 0 && seconds === 10){
+    $("#theClock").css({'margin-left': '-77px', 'font-size':'1080%'});
+    document.getElementById("theClock").innerHTML = seconds;
+  }
+  else if(minutes === 0 && seconds < 10){
+    $("#theClock").css({'margin-left': '-43px', 'font-size':'1020%'});
+    document.getElementById("theClock").innerHTML = seconds;
+  //  $("#theClock").addClass('animated pulse');
+  }
+  else{
+    document.getElementById("theClock").innerHTML = pad(minutes) + ":" + pad(seconds);
+  }
+
 }, 1000);
 }
 
